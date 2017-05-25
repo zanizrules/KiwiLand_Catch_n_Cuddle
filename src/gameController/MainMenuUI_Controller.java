abdd13b3d58034c84caab5aae830023a9df9fc99
Shane Birdsall <zanizrules@gmail.com>
2017-05-25 20:22:31 +1200

    Got scene resizing working in the game

--- src/gameController/MainMenuUI_Controller.java
+++ src/gameController/MainMenuUI_Controller.java
@@ -3,9 +3,12 @@ package gameController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -40,12 +43,31 @@ public class MainMenuUI_Controller {
 
     private void loadFXML(String filePath) throws IOException {
         Stage stage;
-        Parent root;
+        Region root;
+        double sceneWidth = 1280;
+        double sceneHeight = 960;
+
         // get reference to the button's stage
         stage = (Stage) playButton.getScene().getWindow();
         // load up OTHER FXML document
         root = FXMLLoader.load(getClass().getResource(filePath));
-        Scene scene = new Scene(root);
+
+        /*
+            Source for the idea behind the following resize code:
+            http://gillius.org/blog/2013/02/javafx-window-scaling-on-resize.html
+         */
+        if(root.getPrefWidth() == Region.USE_COMPUTED_SIZE) {
+            root.setPrefWidth(sceneWidth);
+        } else sceneWidth = root.getPrefWidth();
+        if(root.getPrefHeight() == Region.USE_COMPUTED_SIZE) {
+            root.setPrefHeight(sceneHeight);
+        } else sceneHeight = root.getPrefHeight();
+
+        Group group = new Group(root);
+        StackPane rootPane = new StackPane(group);
+        Scene scene = new Scene(rootPane);
+        group.scaleXProperty().bind(scene.widthProperty().divide(sceneWidth));
+        group.scaleYProperty().bind(scene.heightProperty().divide(sceneHeight));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,18 +2,39 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/gameView/mainMenuUI.fxml"));
+        Region root = FXMLLoader.load(getClass().getResource("/gameView/mainMenuUI.fxml"));
         primaryStage.setTitle("KiwiLand Catch n Cuddle");
-        primaryStage.setScene(new Scene(root, 1280, 960));
-        primaryStage.setResizable(false);
+
+        double sceneWidth = 1280;
+        double sceneHeight = 960;
+
+        /*
+            Source for the idea behind the following resize code:
+            http://gillius.org/blog/2013/02/javafx-window-scaling-on-resize.html
+         */
+        if(root.getPrefWidth() == Region.USE_COMPUTED_SIZE) {
+            root.setPrefWidth(sceneWidth);
+        } else sceneWidth = root.getPrefWidth();
+        if(root.getPrefHeight() == Region.USE_COMPUTED_SIZE) {
+            root.setPrefHeight(sceneHeight);
+        } else sceneHeight = root.getPrefHeight();
+
+        Group group = new Group(root);
+        StackPane rootPane = new StackPane(group);
+        Scene kiwiLandScene = new Scene(rootPane, sceneWidth, sceneHeight);
+        group.scaleXProperty().bind(kiwiLandScene.widthProperty().divide(sceneWidth));
+        group.scaleYProperty().bind(kiwiLandScene.heightProperty().divide(sceneHeight));
+        primaryStage.setScene(kiwiLandScene);
         primaryStage.show();
     }
 
